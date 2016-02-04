4c2de1035d415ce1aca6e0a6e78207ed4dca2eae
Giso Stallenberg <gisostallenberg@gmail.com>
2016-02-04 17:30:52 +0100

    Add javascript task

---
+++ tasks/javascript.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = function (gulp, config, plugins) {
+    return function () {
+        config.javascript.items.forEach(function(item) {
+
+            if (typeof plugins['include'] == 'undefined') {
+                plugins['include'] = plugins.nop;
+            }
+
+            return gulp.src(item.src)
+                .pipe(plugins.sourcemaps.init())
+                .pipe(plugins.concat(item.outputname))
+                .pipe(plugins.include() )
+                .pipe(
+                    plugins.if(config.production,
+                        plugins.uglify(item.options),
+                        plugins.sourcemaps.write('../maps')
+                    )
+                )
+                .pipe(gulp.dest(item.dest))
+                .pipe(plugins.livereload());
+        });
+    };
+};
