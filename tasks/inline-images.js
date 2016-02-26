c9ceef089a087c4000d0f337cabd35dd4ef12dcd
Arthur Hoek <arthur@connectholland.nl>
2016-02-26 11:20:47 +0100

    Generate a complete scss file, with mixin and classes

--- tasks/inline-images.js
+++ tasks/inline-images.js
@@ -6,6 +6,7 @@
 module.exports = function(gulp, config, plugins) {
     var imageDataURI = require('gulp-image-data-uri');
     var concat = require('gulp-concat');
+    var addsrc = require('gulp-add-src');
 
     return function() {
         return gulp.src(config['inline-images'].src)
@@ -14,7 +15,9 @@ module.exports = function(gulp, config, plugins) {
                     file: __dirname + '/templates/image-data-uri-template.scss'
                 }
             }))
-            .pipe(concat('_inline-images-data.scss'))
+            .pipe(addsrc.prepend(__dirname + '/templates/_inline-images-prepend.scss'))
+            .pipe(addsrc.append(__dirname + '/templates/_inline-images-append.scss'))
+            .pipe(concat('_inline-images.scss'))
             .pipe(gulp.dest('./style/generated'));
     };
 };
