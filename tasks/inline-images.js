b8296211747a881735f96326acad0ba1e9533560
Arthur Hoek <arthur@connectholland.nl>
2016-02-26 13:48:26 +0100

    Add missing map-set() function

--- tasks/inline-images.js
+++ tasks/inline-images.js
@@ -10,13 +10,16 @@ module.exports = function(gulp, config, plugins) {
 
     return function() {
         return gulp.src(config['inline-images'].src)
+            // Create the data uris of the images
             .pipe(imageDataURI({
                 template: {
                     file: __dirname + '/inline-images/image-data-uri-template.scss'
                 }
             }))
+            // Prepend and append scss to make the mixins
             .pipe(addsrc.prepend(__dirname + '/inline-images/_inline-images-prepend.scss'))
             .pipe(addsrc.append(__dirname + '/inline-images/_inline-images-append.scss'))
+            // Output to generated scss file
             .pipe(concat('_inline-images.scss'))
             .pipe(gulp.dest('./style/generated'));
     };
