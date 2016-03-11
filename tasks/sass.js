1db46c9b99eb44564974ca6e1e93f8dbf5529ed8
Arjen Smit <arjen@connectholland.nl>
2016-03-11 16:31:11 +0100

    Add an scss linter that requires the scss_lint gem

--- tasks/sass.js
+++ tasks/sass.js
@@ -1,18 +1,26 @@
 'use strict';
 
 module.exports = function(gulp, config) {
+
+    var shelljs = require('shelljs');
+    var util = require('gulp-util');
+    if (shelljs.which('scss-lint') !== null) {
+        var scssLint = require('gulp-scss-lint');
+    } else {
+        var scssLint = util.noop;
+        util.log(util.colors.red("SCSS linting is disabled"),"to enable this feature please install scss_lint '", util.colors.green("gem install scss_lint"), "'");
+    }
+
     return function() {
 
         /* define required plugins */
         var sourcemaps = require('gulp-sourcemaps');
-        var util = require('gulp-util');
         var sass = require('gulp-sass');
         var plumber = require('gulp-plumber');
         var autoprefixer = require('gulp-autoprefixer');
         var gulpIf = require('gulp-if');
         var moreCss = require('gulp-more-css');
         var livereload = require('gulp-livereload');
-        var scssLint = require('gulp-scss-lint');
 
         /* function to run on execution */
         return gulp.src(config.sass.src)
