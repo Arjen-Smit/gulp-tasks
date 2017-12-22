b61c8c7359fc83d0d1b2a9c8848877b7a71c46b9
Arjen Smit <arjen@connectholland.nl>
2017-12-22 10:55:00 +0100

    make destination of inline images configurable

--- tasks/inline-images.js
+++ tasks/inline-images.js
@@ -10,6 +10,8 @@ module.exports = function(gulp, config) {
         var addSrc = require('gulp-add-src');
         var concat = require('gulp-concat');
 
+        var dest = config['inline-images'].dest || './style/generated';
+
         return gulp.src(config['inline-images'].src)
             // Create the data uris of the images
             .pipe(imageDataUri({
@@ -22,6 +24,6 @@ module.exports = function(gulp, config) {
             .pipe(addSrc.append(__dirname + '/inline-images/_inline-images-append.scss'))
             // Output to generated scss file
             .pipe(concat('_inline-images.scss'))
-            .pipe(gulp.dest('./style/generated'));
+            .pipe(gulp.dest(dest));
     };
 };
